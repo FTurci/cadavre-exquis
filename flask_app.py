f9ea398aaaa1988954377921122d71f5e8da21ff
Francesco Turci <turci@me.com>
2021-09-09 11:52:51 +0200

    form

--- flask_app.py
+++ flask_app.py
@@ -1,14 +1,27 @@
 
-
+# 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
 
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
+
+from wtforms.fields= import IntegerField
+
 
 app = Flask(__name__)
 # app.config['SECRET_KEY'] = 'top-secret!'
 Bootstrap(app)
 
 
-@app.route('/')
+
+class TestForm(FlaskForm):
+    number = IntegerField('Number')
+
+@app.route('/',methods=['POST', 'GET'])
 def index():
-    return render_template('index.html')
+    # level    = IntegerField('User Level', [validators.NumberRange(min=0, max=10)])
+     form = TestForm()
+    if request.method == 'GET':
+        form.number.data = 100
+    if request.method == 'POST':
+        form.number.data = 200
+    return render_template('index.html', form=form)
